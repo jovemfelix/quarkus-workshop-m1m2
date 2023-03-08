08461aa94eaa9455b1a2fe2eef4182a77759942d
Renato Alves Felix <rfelix@redhat.com>
2023-03-08 12:38:42 -0300

    ajuste para facilitar rodar local

--- docs/runlocal.sh
+++ docs/runlocal.sh
@@ -1,16 +1,30 @@
 #!/bin/bash -x
 
-cd /Users/jfalkner/ws/quarkus-workshop/docs
-docker run -it -p 8080:8080 --rm -v $(pwd):/app-data \
--e CHE_URL='http://codeready-codeready.apps.cluster-blr-f777.blr-f777.example.opentlc.com' \
--e CHE_USER_PASSWORD='r3dh4t1!' \
--e CONSOLE_URL='https://console-openshift-console.apps.cluster-blr-f777.blr-f777.example.opentlc.com' \
--e CONTENT_URL_PREFIX="file:///app-data/" \
--e KEYCLOAK_URL='http://keycloak-codeready.apps.cluster-blr-f777.blr-f777.example.opentlc.com' \
--e LOG_TO_STDOUT='true' \
--e MASTER_URL='https://api.cluster-blr-f777.blr-f777.example.opentlc.com:6443' \
--e OPENSHIFT_USER_PASSWORD='r3dh4t1!' \
--e ROUTE_SUBDOMAIN='apps.cluster-blr-f777.blr-f777.example.opentlc.com' \
--e WORKSHOPS_URLS="file:///app-data/_workshop.yml" \
+THIS_SCRIPT=$(basename -- "$0")
+WORKDIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+echo Running $THIS_SCRIPT at $WORKDIR
+
+CHE_URL='http://codeready-codeready.apps.cluster-blr-f777.blr-f777.example.opentlc.com'
+CHE_USER_PASSWORD='r3dh4t1!'
+CONSOLE_URL='https://console-openshift-console.apps.cluster-blr-f777.blr-f777.example.opentlc.com'
+CONTENT_URL_PREFIX="file:///app-data/"
+KEYCLOAK_URL='http://keycloak-codeready.apps.cluster-blr-f777.blr-f777.example.opentlc.com'
+LOG_TO_STDOUT='true'
+MASTER_URL='https://api.cluster-blr-f777.blr-f777.example.opentlc.com:6443'
+OPENSHIFT_USER_PASSWORD='r3dh4t1!'
+ROUTE_SUBDOMAIN='apps.cluster-blr-f777.blr-f777.example.opentlc.com'
+WORKSHOPS_URLS="file:///app-data/_workshop.yml"
+
+docker run -it -p 8080:8080 --rm -v ${WORKDIR}:/app-data \
+-e CHE_URL=${CHE_URL} \
+-e CHE_USER_PASSWORD=${CHE_USER_PASSWORD} \
+-e CONSOLE_URL=${CONSOLE_URL} \
+-e CONTENT_URL_PREFIX=${CONTENT_URL_PREFIX} \
+-e KEYCLOAK_URL=${KEYCLOAK_URL} \
+-e LOG_TO_STDOUT=${LOG_TO_STDOUT} \
+-e MASTER_URL=${MASTER_URL} \
+-e OPENSHIFT_USER_PASSWORD=${OPENSHIFT_USER_PASSWORD} \
+-e ROUTE_SUBDOMAIN=${ROUTE_SUBDOMAIN} \
+-e WORKSHOPS_URLS=${WORKSHOPS_URLS} \
     quay.io/jamesfalkner/workshopper
 
